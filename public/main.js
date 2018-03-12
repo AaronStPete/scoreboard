4bf606eedd4faa2ed7e35d8f6b272744d06177a2
Wasserman <awasserman@proforma.com>
2018-03-12 16:26:18 -0400

    completed explorer mode

--- public/main.js
+++ public/main.js
@@ -11,6 +11,7 @@ console.log("Team 1 Score " + totalScore1);
 console.log("Team 2 Score " + totalScore2);
 
 
+
 const t1Add1 = () => {
   totalScore1++;
   console.log("Team 1 Score " + totalScore1);
@@ -37,4 +38,20 @@ const t2Sub1 = () => {
   console.log("Team 1 Score " + totalScore2);
   const teamScore2 = document.querySelector(".teamScore2");
   teamScore2.textContent = totalScore2;
+}
+
+const newName1 = () => {
+  let name1Field = document.querySelector("#name1Field").value;
+  console.log(name1Field)
+  const jsname1Field = document.querySelector(".team1Name");
+  console.log(jsname1Field)
+  jsname1Field.textContent = name1Field;
+}
+
+const newName2 = () => {
+  let name2Field = document.querySelector("#name2Field").value;
+  console.log(name2Field)
+  const jsname2Field = document.querySelector(".team2Name");
+  console.log(jsname2Field)
+  jsname2Field.textContent = name2Field;
 }
